3c60e586cad109198aab4c7377097af347082799
lyudinha86 <lyudinha712@gmail.com>
2026-07-21 22:57:13 +0100

    Add project files.

---
+++ SuperShop105/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SuperShop105.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
