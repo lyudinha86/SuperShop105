8336a39f36f9de8d1d2dfb1d956d8b0cbce3b53a
lyudinha86 <lyudinha712@gmail.com>
2026-07-24 22:49:13 +0100

    Modify products

--- SuperShop105/Data/Entities/Product.cs
+++ SuperShop105/Data/Entities/Product.cs
@@ -10,6 +10,8 @@ namespace SuperShop105.Data.Entities
 
         public int Id { get; set; }
 
+        [Required]
+        [MaxLength(50, ErrorMessage = "The field {0} can contain at most {1} characters.")]
         public string Name { get; set; }
 
 
@@ -20,11 +22,11 @@ namespace SuperShop105.Data.Entities
         public string ImageUrl { get; set; }
 
         [Display(Name = "Last Purchase")]
-        public DateTime LastPurchase { get; set; }
+        public DateTime? LastPurchase { get; set; }
 
 
         [Display(Name = "Last Sale")]
-        public DateTime LastSale { get; set; }
+        public DateTime? LastSale { get; set; }
 
 
         [Display(Name = "Is Available")]
