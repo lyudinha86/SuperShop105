066e13eea4d1bd185402a9d0fa609af8c14685aa
lyudinha86 <lyudinha712@gmail.com>
2026-07-24 20:33:50 +0100

    First controller

---
+++ SuperShop105/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperShop105.Data.Entities;
+
+namespace SuperShop105.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+
+    }
+
+
+}
