7ef4af0f82f4d227bb4c3ea3426ff9bb40a38266
lyudinha86 <lyudinha712@gmail.com>
2026-09-20 11:57:58 +0100

    Add  items on detail temp.

--- SuperShop105/Data/IOrderRepository.cs
+++ SuperShop105/Data/IOrderRepository.cs
@@ -1,4 +1,5 @@
-﻿using SuperShop105.Data.Entities;
+﻿using SuperShop.Web.Models;
+using SuperShop105.Data.Entities;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,10 @@ namespace SuperShop105.Data
         Task<IQueryable<Order>> GetOrderAsync(string userName);
 
         Task<IQueryable<OrderDetailTemp>> GetDetailTempsAsync(string userName);
+
+        Task AddItemToOrderAsync(AddItemViewModel model, string userName);
+
+        Task ModifyOrderDetailTempQuantityAsync(int id, double quantity);
 
 
     }
